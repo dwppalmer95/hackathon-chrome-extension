3ab15fa8b3c7d7c45d7d5eb93a0b94603d0e9beb
david palmer <dwppalmer95@davids-Air.attlocal.net>
2022-01-13 21:34:02 -0600

    added logic to call dictionary api on selected word.

--- client/main.js
+++ client/main.js
@@ -1,4 +1,21 @@
-const contents = document.getElementById('contents');
-contents.parentNode.removeChild(contents);
-console.log('hackathon test one')
+// const contents = document.getElementById('contents');
+// contents.parentNode.removeChild(contents);
+// console.log('hackathon test one')
 
+
+document.addEventListener('selectionchange', () => { // event listener to check for highlighted text
+    const selString = document.getSelection().toString();
+    if (selString.length > 0){
+      fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + selString)
+        .then(response => response.json())
+        .then(data => console.log(data));
+    }
+});
+
+// Jonathan
+// display the legible string in an html element
+
+// David
+// retrieve legible string from API data
+// set manifest.json to work on any URL
+// data validate the selection string as one word
