b492576d8dc8d5e89a791d78563de9d656e18fc5
david palmer <dwppalmer95@davids-MacBook-Air.local>
2022-01-15 11:19:42 -0600

    cleaned up comments, merged jonathan code

--- client/main.js
+++ client/main.js
@@ -1,15 +1,10 @@
-// const contents = document.getElementById('contents');
-// contents.parentNode.removeChild(contents);
-// console.log('hackathon test one')
-
-// Create and append bubble to be shown 
 let textBubble = document.createElement('div');
 textBubble.setAttribute('class', 'bubble');
 document.body.appendChild(textBubble);
 let mouseX;
 let mouseY;
 let text = document.createTextNode('');
-// event listener to hide our bubble and also grab current mouse position
+
 document.addEventListener('mousedown', (e) => {
   textBubble.style.visibility = 'hidden';
   mouseX = e.clientX;
@@ -23,13 +18,13 @@ document.addEventListener('mousedown', (e) => {
   console.log(document.documentElement.scrollTop);
 })
 
-document.addEventListener('mouseup', () => { // event listener to check for highlighted text
+document.addEventListener('mouseup', () => { 
     const selString = document.getSelection().toString();
     if (selString.length > 0){
       fetch('https://api.dictionaryapi.dev/api/v2/entries/en/' + selString)
         .then(response => response.json())
         .then(data => console.log(data))
-      // move textbubble to selection location and assign text value
+      
       textBubble.style.top = mouseY + document.documentElement.scrollTop + 'px';
       textBubble.style.left = mouseX + 'px';
       textBubble.style.visibility = 'visible'
@@ -39,16 +34,3 @@ document.addEventListener('mouseup', () => { // event listener to check for high
 
     }
 });
-
-// Jonathan
-// display the legible string in an html element
-
-
-// click down -> drag -> click up -> display
-//    event      selectionchange     event 
-
-
-// David
-// retrieve legible string from API data
-// set manifest.json to work on any URL
-// data validate the selection string as one word
